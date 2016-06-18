0edda22304514ef6c4f0cac9ce71390c8f290ee4
Владимир Ершов <Vladimir@MacBook-Pro-Vladimir.local>
2016-06-18 22:24:45 +0200

    We now submit version 1.11

--- manoelgadi12/__init__.py
+++ manoelgadi12/__init__.py
@@ -7,7 +7,9 @@
 
 __author__ = 'ersh'
 __email__ = 'ershov@student.ie.edu'
-__version__ = '1'
+__version__ = '1.111'
+
+import compare_stepwise_genetic
 def hello ():
     print ("Hello World")
 
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='manoelgadi12',
-    version='1.111',
+    version='1.1111',
     description="porject helps to automatically or with human assistance manipulate data",
     long_description=readme + '\n\n' + history,
     author="ersh",
