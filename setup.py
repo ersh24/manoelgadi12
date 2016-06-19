0e4a5fdee768244c5a23059ae428d8fba609fe4f
Владимир Ершов <Vladimir@MacBook-Pro-Vladimir.local>
2016-06-19 21:57:42 +0200

    We now submit version 1.45

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='manoelgadi12',
-    version='1.44',
+    version='1.45',
     description="porject helps to automatically or with human assistance manipulate data",
     long_description=readme + '\n\n' + history,
     author="ersh",
